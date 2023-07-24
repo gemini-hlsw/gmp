ba73fcb0687a5de265f1b16d69732d605684bb73
ekurz-gemini <67289646+ekurz-gemini@users.noreply.github.com>
2023-07-24 12:04:47 -1000

    Create ExecuteFromFileArgument.java

---
+++ giapi-tester/src/main/java/edu/gemini/giapi/tool/arguments/ExecuteFromFileArgument.java
@@ -0,0 +1,32 @@
+package edu.gemini.giapi.tool.arguments;
+
+import edu.gemini.giapi.tool.parser.Argument;
+import edu.gemini.giapi.tool.parser.util.AbstractArgument;
+
+/**
+ * Argument to specify a file containing commands to execute.
+ */
+public class ExecuteFromFileArgument extends AbstractArgument {
+
+    private String fileName;
+
+    public ExecuteFromFileArgument() {
+        super("-file");
+    }
+
+    public boolean requireParameter() {
+        return true;
+    }
+
+    public void parseParameter(String arg) {
+        fileName = arg;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public String getInvalidArgumentMsg() {
+        return "You must specify a filename after the -file argument";
+    }
+}
